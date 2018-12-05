e1398cf5919e75f76804d667da7a4c311511a5d9
Omar Colon <omar.colon@teachforamerica.org>
2018-12-04 22:32:28 -0500

    adds azure meta

--- api/controllers/measurement/capture.js
+++ api/controllers/measurement/capture.js
@@ -1,39 +1,39 @@
 module.exports = {
 
-
   friendlyName: 'Capture',
-
-
   description: 'Capture measurement.',
-
-
   inputs: {
 
   },
-
-
+  
   exits: {
       badRequest: {
         description: 'No payload or deviceId found.',
         responseType: 'badRequest'
+      },
+      unauthorized: {
+        description: '401',
+        responseType: 'unauthorized'
       }
   },
 
-
   fn: async function (inputs, exits) {
 
-    // TODO validate inputs
-    // Install forever
-    // TODO handle errors and return values
     // TODO check API key 
     // deploy to azure service
+    // TODO validate all inputs
+    // Install forever
+    // TODO handle errors and return values
     console.log('about to log request... %s', JSON.stringify(this.req.body));
+    if (!this.req.headers['authorization'] || this.req.headers['authorization'] != 'HJsjs493DXdj392jsSJ20kdjsajsjSW') {
+      return exits.unauthorized();
+    }
 
     if (!this.req.body) {
       console.log("no inputs");
       return exits.badRequest();
     }
-    if (!this.req.body.deviceId) {
+    if (!this.req.body.deviceid) {
       return exits.badRequest("No deviceId found.");
     }
 
@@ -43,6 +43,6 @@ module.exports = {
     const measurement = await Measurement.create(this.req.body).fetch();
     console.log(measurement);
     
-    return exits.success();
+    return exits.success({...measurement});
   }
 };
